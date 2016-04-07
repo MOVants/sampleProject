aa3bfed3f7f53a58d70e00de242f621b5d11893d
MOVants <marvin.villaruel@safesat.com.ph>
2016-04-07 13:57:50 +0800

    MODIFY MESSAGE

--- HeadFirstJava/src/com/arraysDogTest/qa/Dog.java
+++ HeadFirstJava/src/com/arraysDogTest/qa/Dog.java
@@ -27,7 +27,7 @@ public class Dog {
 }
 	public void bark(){
 		System.out.println(name + " says Ruff!");
-		System.out.println(name + " says Ruff!");
+		System.out.println(name + " says EEERR!");
 	}
 		public void eat(){
 			
